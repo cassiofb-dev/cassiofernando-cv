ce081f71969936b79f3ca6f1c83740a02215408e
cassiofb-dev <cassiofb.souza@gmail.com>
2026-09-16 23:03:05 -0300

    feat: add current senior role and enhance the cv

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-tectonic -X compile -Z search-path=. cv/en/cassio-fernando-cv.tex &&
-tectonic -X compile -Z search-path=. cv/pt-br/cassio-fernando-cv.tex
+tectonic -X compile -Z search-path=. cv/en/cassiofernando-cv.tex &&
+tectonic -X compile -Z search-path=. cv/pt-br/cassiofernando-cv.tex
